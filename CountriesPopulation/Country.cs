28de810067c946d7bc8a7a86250065d12d52c6d5
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-04-19 12:06:11 +0530

    Add project files.

---
+++ CountriesPopulation/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountriesPopulation
+{
+    // this is only reader class... contains read-only properties... 
+    class Country
+    {
+        public string Name { get; }
+        public string Code { get; }
+        public int Population { get; }
+
+        public Country(String name, string code, int population)
+        {
+            this.Name = name;
+            this.Code = code;
+            this.Population = population;
+        }
+    }
+}
---
+++ CountriesPopulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CountriesPopulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"F:\PProject\Array\CountriesDemo\CountryData.csv";
+
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+                                                       
+            foreach(Country country in countries)
+            {
+
+                Console.WriteLine($"{country.Population} : {country.Name}");
+            }
+
+        }
+    }
+}
